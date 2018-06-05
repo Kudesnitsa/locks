d8ee70d24d1230eafde403e8345f90f216b0a313
Kudesnitsa <dl11021997@gmail.com>
2018-06-05 16:36:38 +0300

    edit index

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 $dirs = array_filter(glob('*'), 'is_dir');
 //print_r( $dirs);
-
-echo "<h1>НАШИ ПРОЕКТЫ ПО ВЕРТСКЕ</h1>";
 foreach($dirs as $dir){
   echo '<a href = "/'.$dir.'">'.$dir.'</a><br />';
 }
